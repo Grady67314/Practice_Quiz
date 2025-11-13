4aca281efe418e9c58b386dc44020353d5f243e3
Grady67314 <gradmcev099@ed.amdsb.ca>
2025-11-13 10:29:53 -0500

    g

--- Practice_Quiz/Form1.cs
+++ Practice_Quiz/Form1.cs
@@ -21,5 +21,32 @@ namespace Practice_Quiz
         {
 
         }
+
+        private void question01Button_Click(object sender, EventArgs e)
+        {
+            question1OutputLabel.Text = "Happy Hump Day ";
+
+        }
+
+        private void question2Button_Click(object sender, EventArgs e)
+        {
+            string username = question2TextBox.Text;
+            question2OutputLabel.Text = "Hello " + username + ".";
+        }
+
+        private void question3Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int int1 = Convert.ToInt16(q3Int1Text.Text);
+                int int2 = Convert.ToInt16(q3int2Text.Text);
+                int product = int1 * int2;
+                question3OutputLabel.Text = int1 +" X "+ int2 +" = " + product;
+            }
+            catch
+            {
+                question3OutputLabel.Text = "Please enter an integer and try again.";
+            }
+        }
     }
 }
